eac742366fccb693e6a5d344a7f9636c9f8dea95
khaibtros <trankhai15052005@gmail.com>
2025-03-13 00:07:45 +0700

    minor fix

--- src/java/Controll/LogoutController.java
+++ src/java/Controll/LogoutController.java
@@ -62,7 +62,7 @@ public class LogoutController extends HttpServlet {
         if (session != null) {
             session.invalidate();
         }
-        response.sendRedirect(request.getContextPath());
+        response.sendRedirect(request.getContextPath() + "/login");
     }
 
     /**
